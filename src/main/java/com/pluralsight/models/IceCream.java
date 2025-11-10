72a4fa4e365a41bb4a6333325b472e5003b86d82
Amaan0107 <Adilawar2007@gmail.com>
2025-11-10 10:14:55 -0800

    starting of order class More work to be done :)

--- src/main/java/com/pluralsight/models/IceCream.java
+++ src/main/java/com/pluralsight/models/IceCream.java
@@ -1,11 +1,18 @@
 package com.pluralsight.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IceCream extends MenuItem {
     private String container;
+    private int scoops;
+    private List<Topping> toppings;
 
     public IceCream(String name, double price, String size, String container) {
         super(name, price, size);
         this.container = container;
+        this.scoops = scoops;
+        this.toppings = new ArrayList<>();
     }
 
     public String getContainer() {
--- src/main/java/com/pluralsight/util/Order.java
+++ src/main/java/com/pluralsight/util/Order.java
@@ -1,4 +1,13 @@
 package com.pluralsight.util;
+import com.pluralsight.models.IceCream;
+import com.pluralsight.models.IceCreamCake;
+import com.pluralsight.models.Topping;
+import com.pluralsight.models.Smoothie;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Order {
+
 }
