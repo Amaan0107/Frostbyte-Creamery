fbbd50a1beae173db8eca8f434ba706ad4fe98db
Amaan0107 <Adilawar2007@gmail.com>
2025-11-10 10:35:53 -0800

    making slight changes with help

--- src/main/java/com/pluralsight/util/Order.java
+++ src/main/java/com/pluralsight/util/Order.java
@@ -15,30 +15,20 @@ public class Order {
         menuItems = new ArrayList<>();
         orderDate = LocalDateTime.now();
     }
-    public void addIceCream(IceCream iceCream) {
-        iceCreams.add(iceCream);
+    public void addMenuItem(MenuItem menuItem) {
+        menuItems.add(menuItem);
     }
-    public void addSmoothie(Smoothie smoothie) {
-        smoothies.add(smoothie);
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
     }
-    public void addTopping(Topping topping) {
-        toppings.add(topping);
+    public boolean hasMenuItem(MenuItem menuItem) {
+        return menuItems.contains(menuItem);
     }
-    public void addIceCreamCake(IceCreamCake iceCreamCake) {
-        iceCreamCakes.add(iceCreamCake);
+    public LocalDateTime getOrderDate() {
+        return orderDate;
     }
-    public List<IceCream> getIceCreams() {
-        return iceCreams;
+    public double CalculatePrice() {
+        return menuItems.stream().mapToDouble(MenuItem::getPrice).sum();
     }
-    public List<Smoothie> getSmoothies() {
-        return smoothies;
-    }
-    public List<Topping> getToppings() {
-        return toppings;
-    }
-    public List<IceCreamCake> getIceCreamCakes() {
-        return iceCreamCakes;
-    }
-    boolean has
 
 }
