23974d7fe9cb139bf1acdcd2a26f8e66b73c15b1
Amaan0107 <Adilawar2007@gmail.com>
2025-11-12 12:07:20 -0800

    adding test to order class methods

--- src/test/java/com/pluralsight/util/OrderTest.java
+++ src/test/java/com/pluralsight/util/OrderTest.java
@@ -1,7 +1,22 @@
 package com.pluralsight.util;
 
+import com.pluralsight.models.MenuItem;
+import org.junit.jupiter.api.Test;
 import static org.junit.Assert.*;
 
 public class OrderTest {
+    @Test
+    void testAddMenuItemAndCalculatePrice() {
+        Order order = new Order();
+        MenuItem item1 = new MenuItem("Ice Cream", 3.50, "Small");
+        MenuItem item2 = new MenuItem("Smoothie", 5.55, "Small");
+
+        order.addMenuItem(item1);
+        order.addMenuItem(item2);
+
+        assertEquals(2, order.getMenuItems().size());
+        assertTrue(order.hasMenuItem(item1));
+        assertEquals(9.05, order.CalculatePrice(), 0.001);
+    }
 
 }
