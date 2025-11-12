126090c3e23433203b0469dba80c6ea161f24ad9
Amaan0107 <Adilawar2007@gmail.com>
2025-11-12 12:19:11 -0800

    adding test to receipt class methods

--- src/test/java/com/pluralsight/util/ReceiptTest.java
+++ src/test/java/com/pluralsight/util/ReceiptTest.java
@@ -1,7 +1,23 @@
 package com.pluralsight.util;
 
+import com.pluralsight.models.MenuItem;
+import org.junit.jupiter.api.Test;
+import java.io.File;
 import static org.junit.Assert.*;
 
 public class ReceiptTest {
 
+    @Test
+    void testSaveReceiptCreatesFile() {
+        Order order = new Order();
+        MenuItem item = new MenuItem("Ice Cream", 3.5, "Small");
+        order.addMenuItem(item);
+
+        Receipt receipt = new Receipt(order);
+        receipt.saveReceipt();
+
+        File file = new File("receipt.txt");
+        assertTrue(file.exists());
+        assertTrue(file.length() > 0);
+    }
 }
