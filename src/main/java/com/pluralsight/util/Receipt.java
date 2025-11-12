3fd4b1861e9694e4d3fb13924ee8a6a43e1f88ae
Amaan0107 <Adilawar2007@gmail.com>
2025-11-11 16:52:18 -0800

    almost done with Ui working on receipt

--- src/main/java/com/pluralsight/util/Receipt.java
+++ src/main/java/com/pluralsight/util/Receipt.java
@@ -1,4 +1,14 @@
 package com.pluralsight.util;
 
+import com.pluralsight.models.MenuItem;
+import java.io.File;
+import java.io.FileWriter;
+import java.time.format.DateTimeFormatter;
+
 public class Receipt {
+    private Order order;
+
+    public Receipt(Order order) {
+        this.order = order;
+    }
 }
