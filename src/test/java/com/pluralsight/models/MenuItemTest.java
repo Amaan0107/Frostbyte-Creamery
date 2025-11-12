497f67925aef6a398f233a6556f52d7b8c2db2f2
Amaan0107 <Adilawar2007@gmail.com>
2025-11-12 11:45:54 -0800

    adding tests for menuitem class

--- src/test/java/com/pluralsight/models/MenuItemTest.java
+++ src/test/java/com/pluralsight/models/MenuItemTest.java
@@ -1,7 +1,15 @@
 package com.pluralsight.models;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MenuItemTest {
+    @Test
+    public void testConstructorAndGetters() {
+        MenuItem item = new MenuItem("Ice Cream", 3.50, "Small");
+        assertEquals("Ice Cream", item.getName());
+        assertEquals(3.50, item.getPrice());
+        assertEquals("Small", item.getSize());
+    }
 
 }
